0a4c8b147fe08bd3601544cb7494000c3d697b52
lemon <chiachingman1@gmail.com>
2021-03-20 18:50:03 +1000

    Add project files.

---
+++ Intersection of Two Linked Lists/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Intersection_of_Two_Linked_Lists
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+  }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world...");
+        }
+
+
+        public static ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+        {
+            if (headA==null||headB==null)
+            {
+                return null;
+            }
+            ListNode pointerA = headA;
+            ListNode pointerB = headB;
+
+            while (pointerA!=pointerB)
+            {
+                pointerA = pointerA == null ? headB :  pointerA.next;
+
+                pointerB = pointerB == null ? headA : pointerB.next;
+            }
+            return pointerA;
+        }
+    }
+}
